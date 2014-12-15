2f9c198908f96e8d7a18a296c791d64f29a0932a
ddunn801 <ddunn801@gmail.com>
2014-12-14 18:06:20 -0600

    shiny complete; first commit for Rpres

--- server.R
+++ server.R
@@ -1,18 +1,19 @@
-# Define server logic required to draw a histogram
+# Prepare -----------------------------------------------------------------
+pkg <- c("shiny")
+inst <- pkg %in% installed.packages()
+if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
+lapply(pkg,library,character.only=TRUE)
+rm(inst,pkg)
+
+
+# Define server logic required to draw a plot
 shinyServer(function(input, output) {
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-  output$distPlot <- renderPlot({
-    x    <- faithful[, 2]  # Old Faithful Geyser data
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  output$predPlot <- renderPlot({
+    pred1 <- round(43.00103 + (-11.72829*(input$Cause==2)) + (-0.05837*input$sDay),0)
+    Season <- c(7,4,39,11,13,5,115,2,2,5,7,21,187,13,38,4,2,13,1,1,4,2,17,8,15,97,89,36,145,19,11,13,5,1,14,13,153,59,4,19,43,21,4,22,6,53,9,6,6,4,11,5,28,22,396,30,8,2,2,23,65,51,82,8,32,5,10,22,13,20,59,9,1,3,2,2,13,85,3,21,191,282,18,2,18,5,4,60,11,13,46,5,4,2)
+    hist(Season,breaks=60,xlab="Days Out",col="blue",
+         main=paste0("Histogram of 2013-14 Injuries \n",pred1," Days Predicted for Injury"))
+    abline(v=pred1,col="red",lwd=5)
   })
 })
--- ui.R
+++ ui.R
@@ -1,23 +1,29 @@
+# Prepare -----------------------------------------------------------------
+pkg <- c("shiny")
+inst <- pkg %in% installed.packages()
+if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
+lapply(pkg,library,character.only=TRUE)
+rm(inst,pkg)
+
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Hello Shiny!"),
+  titlePanel("Injury Effect Predictor"),
   
-  # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
+      selectInput(inputId="Cause",
+                  label=h3("Injury Caused During:"),
+                  choices=list("Match"=1,"Training"=2)),
+      sliderInput(inputId="sDay",
+                   label=h3("Days into the Season:"),
+                   min=0,max=200,step=10,value=30)),
     mainPanel(
-      plotOutput("distPlot")
+      h5("Instructions:  This application predicts how long a soccer player will be out for injury, given two inputs:  whether the injury occurred in training/match & how far into the season the injury happened. The model is built on actual results from Arsenal Football Club's 2013 season. The histogram shows all injuries, for context. The red line is the expected number of days out for your scenario."),
+      plotOutput("predPlot")
+      )
     )
-  )
 ))
 
